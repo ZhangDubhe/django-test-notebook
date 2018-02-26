8f4814ecd18793fd899c61275eb6b215063a4797
Dubhee <zht_dubhe@163.com>
2018-02-26 15:54:41 -0700

    更改管理员管理界面

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Question
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User',             {'fields': ['user_name']}),
+        ('User information', {'fields': ['user_email'], 'classes': ['collapse']}),
+    ]
+    list_display = ('user_name','user_email','add_at','is_admin')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Question)
