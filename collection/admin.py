37813eb12aed7b84a665feb13a21d33e66bab801
Dubhee <zht_dubhe@163.com>
2018-02-26 16:00:25 -0700

    更新管理员更改的方式

--- collection/admin.py
+++ collection/admin.py
@@ -7,7 +7,7 @@ from .models import User, Question
 class UserAdmin(admin.ModelAdmin):
     fieldsets = [
         ('User',             {'fields': ['user_name']}),
-        ('User information', {'fields': ['user_email'], 'classes': ['collapse']}),
+        ('User information', {'fields': ['user_email','user_password','is_admin'], 'classes': ['collapse']}),
     ]
     list_display = ('user_name','user_email','add_at','is_admin')
 
