203d7f5724d54a99bfa5a9de49f308fca24022e4
Dubhee <zht_dubhe@163.com>
2018-03-08 23:26:22 -0700

    Click Right answer

--- static/js/load.js
+++ static/js/load.js
@@ -21,6 +21,20 @@ $(function () {
 		control_panel();
 	});
 
+	$('.answer-card').click(function(){
+		var if_edit =  $(this).attr('contenteditable');
+		var select_ans = $(this).text();
+		if(if_edit == undefined){
+			console.log(select_ans,session.answer);
+			if(select_ans == session.answer){
+				console.log('right');
+				$(this).siblings('.answer-card').hide();
+			}else{
+				$(this).addClass('wrong');
+			}
+			// $(".align-left").html('<a href="'API_PATH+u/'"></a>')
+		}
+	})
 })
 
 if($('.question-header')){
